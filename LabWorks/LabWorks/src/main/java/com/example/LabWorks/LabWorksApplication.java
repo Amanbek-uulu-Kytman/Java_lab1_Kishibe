32e1ca32b75996323dd727f0832f9ff9861546ce
Amanbek-uulu-Kytman <gidhubkytmanbek@mail.ru>
2023-02-19 14:03:18 +0600

    Initial commit

---
+++ LabWorks/LabWorks/src/main/java/com/example/LabWorks/LabWorksApplication.java
@@ -0,0 +1,13 @@
+package com.example.LabWorks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabWorksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabWorksApplication.class, args);
+	}
+
+}
